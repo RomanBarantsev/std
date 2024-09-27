3448fff4f1fb567dc610b129bf7b69851b5fdd76
Roman <roman.it.kirov@yandex.ru>
2024-09-27 15:10:11 +0300

    1.2.0
    list+deque

--- std/std/std.cpp
+++ std/std/std.cpp
@@ -3,8 +3,21 @@
 
 #include <iostream>
 #include "myLib.h"
+#include <list>
 
 int main()
 {
-
+	myLib::List<int> L{ 1,2,3 };
+	L.push
+	std::list<int> L2{ 1,2,3 };
+	L2.push_back(1);
+	//std::cout << L2.size() << std::endl;
+	{
+		myLib::LogDuration l("1");
+		std::list<int> L2(1e7,0);
+	}
+	{
+		myLib::LogDuration l("3");
+		myLib::ListAI<int> L(1e7, 0);
+	}
 }
