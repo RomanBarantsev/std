fbd966107319941db5c80f05c0c8b7c75837032b
Roman <roman.it.kirov@yandex.ru>
2024-11-04 19:12:25 +0300

    1.2.1
    fixing mistakes in Vector, such as:
    1. Wrong indexing in push_back
    2. Memory leak in push_back
    3. capacity growth.
    4.Assignment operator check for self-assignment.
    5.delete[] in Destruction

---
+++ std/std.cpp
@@ -0,0 +1,28 @@
+﻿// std.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <deque>
+#include "myLib.h"
+#include "g:\Git\UsefulLibrary\UsefulLibrary\UsefulLibrary.h"
+
+int main()
+{
+	myLib::Vector<int> v1{1,2,3};
+	myLib::Vector<int> v2;
+	v2 = v1;
+	myLib::Vector<int> v3;
+	v3 = std::move(v1);
+	myLib::Vector<int> v4 = std::move(v3);	
+	for (size_t i = 0; i < v2.getSize(); i++)
+	{		
+		std::cout << (v2[i]) << std::endl;		
+	}
+	std::deque<long long> deq{ 1,2,3 };
+	std::vector<int> vec(1000, 1000);
+	deq.push_back(1);
+	std::vector<int> vec2(1000, 1000);
+	deq.push_back(1);
+	Usefull::isFragmented(deq);
+}
