8af3040fe9aa102c0a2fe40a24b6a8da3052db6a
Roman <roman.it.kirov@yandex.ru>
2024-11-23 20:23:21 +0300

    1.1.0
    moved to it's own h and cpp.

--- std/std.cpp
+++ std/std.cpp
@@ -6,25 +6,14 @@
 #include <deque>
 #include "myLib.h"
 #include <list>
+#include "List.h"
 //#include "g:\Git\UsefulLibrary\UsefulLibrary\UsefulLibrary.h"
 
 int main()
 {
 	std::list<int> li{1,2,3};
 	li.assign({ 4,5,6 });
-	myLib::List<int> l1{1,2,3};
-	myLib::List<int> l3{1,2,3};
-	l1.push_back(4);
-	l1.push_front(0);
-	l3 = l1;
-	/*l3.clear();
-	{
-		myLib::List<long long> l{ 100000 };
-		std::vector<myLib::List<long long>> ll(10000000, l);
+	List<int> l{ 1,2,3 };
 
-	}*/
-	
-	/*l1.push_back(4);
-	l1.push_front(0);*/
 
 }
