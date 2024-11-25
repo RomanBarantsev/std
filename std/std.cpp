f976c8854ebf2afa80557d627a6644f3ac671535
RomanBarantsev <90321836+RomanBarantsev@users.noreply.github.com>
2024-11-25 14:15:43 +0300

    1.3.0
    Moving to h and cpp.

--- std/std.cpp
+++ std/std.cpp
@@ -6,17 +6,16 @@
 #include <deque>
 #include "myLib.h"
 #include <list>
+#include "MyList.h"
 //#include "g:\Git\UsefulLibrary\UsefulLibrary\UsefulLibrary.h"
 
 int main()
 {
 	std::list<int> li{1,2,3};
 	li.assign({ 4,5,6 });
-	myLib::List<int> l1{1,2,3};
-	myLib::List<int> l3{1,2,3};
+	List<int> l1{1,2,3};
 	l1.push_back(4);
 	l1.push_front(0);
-	l3 = l1;
 	/*l3.clear();
 	{
 		myLib::List<long long> l{ 100000 };
